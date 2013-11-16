545f4698377b33e1dc2e14bb69835af008ee295a
Florian Heiber <flo@flohei.de>
2013-11-16 14:48:22 +0100

    cleanup

--- builds.sh
+++ builds.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 buildNumber=$(curl -s http://yourdomain.com/builds.php?app=$1)
-echo $buildNumber
 /usr/libexec/PlistBuddy -c "Set :CFBundleVersion $buildNumber" ./YourApp/YourApp-Info.plist
 echo $buildNumber
